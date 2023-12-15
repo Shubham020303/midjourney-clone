9de578a7b9e553a7e91350153ac035de3a2dd5fa
Shubham020303 <72245331+Shubham020303@users.noreply.github.com>
2023-12-15 10:59:03 +0530

    Update index.js and added cors

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,10 @@ import dalleRoutes from "./routes/dalleRoutes.js"
 dotenv.config()
 
 const app = express()
-app.use(cors())
 app.use(express.json({ limit: "50mb" }))
+app.use(cors({
+     origin: 'https://midjarni.netlify.app/',
+}));
 
 app.use("/api/v1/post", postRoutes)
 app.use("/api/v1/dalle", dalleRoutes)
