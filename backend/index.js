5ce0f9095f3482f5074c2913a1987081ee9ab989
Shubham020303 <72245331+Shubham020303@users.noreply.github.com>
2023-12-15 11:02:26 +0530

    Update index.js removed cors

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,7 @@ dotenv.config()
 
 const app = express()
 app.use(express.json({ limit: "50mb" }))
-app.use(cors({
-     origin: 'https://midjarni.netlify.app/',
-}));
+app.use(cors());
 
 app.use("/api/v1/post", postRoutes)
 app.use("/api/v1/dalle", dalleRoutes)
