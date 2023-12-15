c78724bbee6525ac8d8a14a56de1df04d3974336
Shubham020303 <72245331+Shubham020303@users.noreply.github.com>
2023-12-15 11:37:36 +0530

    Update index.js added options

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ dotenv.config()
 const app = express()
 app.use(express.json({ limit: "50mb" }))
 app.use(cors());
+app.options('*', cors());
 
 app.use("/api/v1/post", postRoutes)
 app.use("/api/v1/dalle", dalleRoutes)
