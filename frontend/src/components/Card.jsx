ec24ae64b35c254a3bf0236bac320433efba2734
shubhambhogayata <shubham@pruthatek.com>
2023-12-19 11:54:34 +0530

    updated backend

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,7 +5,7 @@ import { downloadImage } from '../utils'
 const Card = ({ _id, name, prompt, photo }) => {
 	return (
 		<div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
-			<img src={photo} className='w-full h-auto object-cover rounded-xl' alt={prompt} loading='lazy' />
+			<img src={photo} className='w-full h-full object-cover rounded-xl' alt={prompt} loading='lazy' />
 			<div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
 				<p className='text-white text-md overflow-y-auto prompt'>{prompt}</p>
 				<div className='mt-5 flex justify-between items-center gap-2'>
